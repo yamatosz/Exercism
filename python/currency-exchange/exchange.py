eec5d84a77041fdce85ba95e47628c4baa6316a9
yamatosz <lucasj191218@gmail.com>
2022-10-08 16:25:56 -0300

    modify in exchange.py

--- python/currency-exchange/exchange.py
+++ python/currency-exchange/exchange.py
@@ -5,7 +5,7 @@ def exchange_money(budget, exchange_rate):
     :param exchange_rate: float - unit value of the foreign currency.
     :return: float - exchanged value of the foreign currency you can receive.
     """
-    return (budget / exchange_rate)
+    return budget / exchange_rate
 
 
 def get_change(budget, exchanging_value):
