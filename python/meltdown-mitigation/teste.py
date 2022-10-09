ae9d827b97e19dd179659f2a53e755b4489719d6
yamatosz <lucasj191218@gmail.com>
2022-10-09 00:46:02 -0300

    add meltdown-mitigation

---
+++ python/meltdown-mitigation/teste.py
@@ -0,0 +1,5 @@
+from conditionals import is_criticality_balanced, reactor_efficiency, fail_safe
+
+print(is_criticality_balanced(750, 600))
+print(reactor_efficiency(10, 0, 10000))
+print(fail_safe(1000,30,5000))
