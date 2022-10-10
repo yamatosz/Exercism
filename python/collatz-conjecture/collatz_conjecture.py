3cf6fd025f4eba99780e316e755d1f60529304c4
yamatosz <lucasj191218@gmail.com>
2022-10-09 22:09:12 -0300

    modifify collatz-conjecture

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -2,12 +2,13 @@ def steps(number):
     if number <= 0:
         raise ValueError('Only positive integers are allowed')
     else:
+        numbers = [number]
         n = 0
 
         while number != 1:
             n+=1
             if number % 2 == 0:
                 number = number/2
-                continue
-            number = 3*number + 1
-        return n
+            else: number = 3*number + 1
+            numbers.append(number)
+        return n, numbers
--- python/collatz-conjecture/teste.py
+++ python/collatz-conjecture/teste.py
@@ -1,6 +1,15 @@
 from collatz_conjecture import steps
+import matplotlib.pyplot as plt
+import numpy as np
 
 try:
-    print(steps(0))
+    _steps = steps(500)
 except ValueError as e:
     print(e)
+
+x = np.arange(0, _steps[0] + 1)
+plt.plot(x, _steps[1])
+plt.xlabel('Numeros de interação')
+plt.ylabel('Numeros')
+plt.grid()
+plt.show()
