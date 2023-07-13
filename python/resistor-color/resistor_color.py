cf1e9c6c8ee14311c07429b51e12b346601dfc03
Lucas Jose <90628627+yamatosz@users.noreply.github.com>
2023-07-13 12:09:44 -0300

    add resistor-color py

---
+++ python/resistor-color/resistor_color.py
@@ -0,0 +1,10 @@
+def color_code(color):
+    _colors = ['black', 'brown', 'red', 'orange', 'yellow',
+               'green', 'blue', 'violet', 'grey', 'white']
+    return _colors.index(color)
+
+
+def colors():
+    _colors = ['black', 'brown', 'red', 'orange', 'yellow',
+               'green', 'blue', 'violet', 'grey', 'white']
+    return _colors
